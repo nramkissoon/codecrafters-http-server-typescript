f6a0205663c340af2e95aa950fd7ed72554c05a7
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-06-20 14:28:19 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,14 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     const path = request.split(" ")[1];
+    if (path.startsWith("/echo/")) {
+      const echo = path.split("/")[2];
+      const len = echo.length;
+      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${len}\r\n\r\n${echo}`;
+      socket.write(response);
+      socket.end();
+      return;
+    }
     const response =
       path === "/"
         ? "HTTP/1.1 200 OK\r\n\r\n"
