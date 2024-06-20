18fa03389ae8e5ffda093caa05e722a922c8e2ce
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-06-20 14:20:15 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,14 @@
 import * as net from "net";
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n" + "\r\n");
-  socket.on("close", () => {
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const path = request.split(" ")[1];
+    const response =
+      path === "/"
+        ? "HTTP/1.1 200 OK\r\n\r\n"
+        : "HTTP/1.1 404 Not Found\r\n\r\n";
+    socket.write(response);
     socket.end();
   });
 });
