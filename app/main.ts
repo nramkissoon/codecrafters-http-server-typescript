f87755342a49ab439139f4016817f014cf2487c5
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-06-25 13:09:28 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,20 +3,39 @@ import * as net from "net";
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
+    console.log(request);
     const path = request.split(" ")[1];
-    if (path.startsWith("/echo/")) {
-      const echo = path.split("/")[2];
-      const len = echo.length;
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${len}\r\n\r\n${echo}`;
+    console.log(path.split("/")[1]);
+    const params = path.split("/")[1];
+    let response: string;
+    function changeResponse(response: string): void {
       socket.write(response);
       socket.end();
-      return;
     }
-    const response =
-      path === "/"
-        ? "HTTP/1.1 200 OK\r\n\r\n"
-        : "HTTP/1.1 404 Not Found\r\n\r\n";
-    socket.write(response);
+    switch (params) {
+      case "": {
+        response = "HTTP/1.1 200 OK\r\n\r\n";
+        changeResponse(response);
+        break;
+      }
+      case "echo": {
+        const message = path.split("/")[2];
+        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
+        changeResponse(response);
+        break;
+      }
+      case "user-agent": {
+        const userAgent = request.split("User-Agent: ")[1].split("\r\n")[0];
+        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+        changeResponse(response);
+        break;
+      }
+      default: {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        changeResponse(response);
+        break;
+      }
+    }
     socket.end();
   });
 });
