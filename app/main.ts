fcc47c283b7e803a87eece5d9aba27ff46698038
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-06-25 14:00:39 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,25 @@
 import * as net from "net";
 import fs from "fs";
 
+const getAcceptEncoding = (request: string) => {
+  const encodings = request.split("Accept-Encoding: ")[1];
+  if (encodings) {
+    return encodings.split("\r\n")[0].split(",");
+  }
+};
+
+const buildResponseHeaders = (headers?: string[], compress?: boolean) => {
+  if (compress) {
+    headers?.push("Content-Encoding: gzip");
+  }
+  if (!headers) return "\r\n";
+  return headers.join("\r\n") + "\r\n\r\n";
+};
+
+const OK = "HTTP/1.1 200 OK";
+const CREATED = "HTTP/1.1 201 Created";
+const NOT_FOUND = "HTTP/1.1 404 Not Found";
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
@@ -9,26 +28,45 @@ const server = net.createServer((socket) => {
     console.log(path.split("/")[1]);
     const params = path.split("/")[1];
     let response: string;
-    function changeResponse(response: string): void {
+    function changeResponse(
+      status: string,
+      headers?: string[],
+      body?: string
+    ): void {
+      const acceptEncodings = getAcceptEncoding(request);
+      let compress = false;
+      if (acceptEncodings?.find((v) => v === "gzip")) {
+        compress = true;
+      }
+      const response = `${status}\r\n${buildResponseHeaders(
+        headers,
+        compress
+      )}${body}`;
       socket.write(response);
       socket.end();
     }
     switch (params) {
       case "": {
-        response = "HTTP/1.1 200 OK\r\n\r\n";
-        changeResponse(response);
+        changeResponse(OK);
         break;
       }
       case "echo": {
         const message = path.split("/")[2];
         response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
-        changeResponse(response);
+        changeResponse(
+          OK,
+          ["Content-Type: text/plain", `Content-Length: ${message.length}`],
+          message
+        );
         break;
       }
       case "user-agent": {
         const userAgent = request.split("User-Agent: ")[1].split("\r\n")[0];
-        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-        changeResponse(response);
+        changeResponse(
+          OK,
+          ["Content-Type: text/plain", `Content-Length: ${userAgent.length}`],
+          userAgent
+        );
         break;
       }
       case "files": {
@@ -40,22 +78,27 @@ const server = net.createServer((socket) => {
           const lines = request.split("\r\n");
           const requestBody = lines[lines.length - 1];
           fs.writeFileSync(filePath, requestBody);
-          changeResponse("HTTP/1.1 201 Created\r\n\r\n");
+          changeResponse(CREATED);
           break;
         }
         if (fs.existsSync(filePath)) {
           const contents = fs.readFileSync(filePath);
-          response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${contents.length}\r\n\r\n${contents}`;
-          changeResponse(response);
+          changeResponse(
+            OK,
+            [
+              "Content-Type: application/octet-stream",
+              `Content-Length: ${contents.length}`,
+            ],
+            `${contents}`
+          );
           break;
         } else {
-          changeResponse("HTTP/1.1 404 Not Found\r\n\r\n");
+          changeResponse(NOT_FOUND);
           break;
         }
       }
       default: {
-        response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        changeResponse(response);
+        changeResponse(NOT_FOUND);
         break;
       }
     }
